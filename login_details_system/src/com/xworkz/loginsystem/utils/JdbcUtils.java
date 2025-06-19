5dec49c061d0b45862e9f55433d2a5e72715ba64
Damish-7 <damishperaje9@gmail.com>
2025-06-19 23:00:22 +0530

    log

---
+++ login_details_system/src/com/xworkz/loginsystem/utils/JdbcUtils.java
@@ -0,0 +1,14 @@
+package com.xworkz.loginsystem.utils;
+
+public class JdbcUtils {
+
+    private JdbcUtils(){
+
+    }
+
+    public static final String jdbcUrl="jdbc:mysql://localhost:3306/xworkz";
+    public static final String jdbcUserName ="root";
+    public static final String jdbcPassword="8970020170D@d";
+    public static final String jdbcDriver ="com.mysql.cj.jdbc.Driver";
+
+}
